78e2b4e6c77d32164f4518dacf89bf12902c0ab1
Arjun Kandel <75780673+arjunkandel2000@users.noreply.github.com>
2022-06-27 12:46:50 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62a6caf649bae72e7ce270339cba57ab.js"
+  "/Naxa-Frontend-Page/precache-manifest.1be2a477ca1f3a922f65cb3105f24c28.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Naxa-Frontend-Page/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
