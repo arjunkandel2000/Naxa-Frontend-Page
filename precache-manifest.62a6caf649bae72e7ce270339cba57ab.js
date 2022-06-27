89fe69bb4e7e16d10ea0a3501990d443fc3cd561
Arjun Kandel <75780673+arjunkandel2000@users.noreply.github.com>
2022-06-27 12:35:50 +0545

    Updates

---
+++ precache-manifest.62a6caf649bae72e7ce270339cba57ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5787a47edaaaf69f76d",
+    "url": "/static/css/main.f37dbeeb.chunk.css"
+  },
+  {
+    "revision": "b5787a47edaaaf69f76d",
+    "url": "/static/js/main.b5787a47.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "c56d1fd8005f74df8840",
+    "url": "/static/js/2.c56d1fd8.chunk.js"
+  },
+  {
+    "revision": "526e76a96793b81fba07",
+    "url": "/static/js/runtime~main.526e76a9.js"
+  },
+  {
+    "revision": "fd87d179b8fca3c7d42b8cc42ac9eede",
+    "url": "/index.html"
+  }
+];
